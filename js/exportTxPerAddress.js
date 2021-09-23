aaeb7eeda8eb9a956a0000fab63dcc08aa061b12
Simone Romano <mail@sromano.com>
2021-09-23 12:46:51 +0200

    added more scripts

---
+++ js/exportTxPerAddress.js
@@ -0,0 +1,48 @@
+const Web3 = require("web3");
+
+const web3 = new Web3('https://babel-api.mainnet.iotex.io');
+
+
+run();
+
+async function run() {
+    getTransactionsByAccount("0x6b132450C6988246cf60501f37CdF7eEd5d19176", 13334400, 13354920)
+}
+
+async function getTransactionsByAccount(myaccount, startBlockNumber, endBlockNumber) {
+    if (endBlockNumber == null) {
+      endBlockNumber = web3.eth.blockNumber;
+      console.log("Using endBlockNumber: " + endBlockNumber);
+    }
+    if (startBlockNumber == null) {
+      startBlockNumber = endBlockNumber - 1000;
+      console.log("Using startBlockNumber: " + startBlockNumber);
+    }
+    console.log("Searching for transactions to/from account \"" + myaccount + "\" within blocks "  + startBlockNumber + " and " + endBlockNumber);
+  
+    for (var i = startBlockNumber; i <= endBlockNumber; i++) {
+      if (i % 100 == 0) {
+        console.log(new Date()+23.getSeconds()+" Searching block " + i);
+      }
+      var block = await web3.eth.getBlock(i, true);
+      if (block != null && block.transactions != null) {
+        block.transactions.forEach( function(e) {
+          if (myaccount == "*" || myaccount == e.from || myaccount == e.to) {
+            console.log("  tx hash          : " + e.hash + "\n"
+              + "   nonce           : " + e.nonce + "\n"
+              + "   blockHash       : " + e.blockHash + "\n"
+              + "   blockNumber     : " + e.blockNumber + "\n"
+              + "   transactionIndex: " + e.transactionIndex + "\n"
+              + "   from            : " + e.from + "\n" 
+              + "   to              : " + e.to + "\n"
+              + "   value           : " + e.value + "\n"
+              + "   time            : " + block.timestamp + " " + new Date(block.timestamp * 1000).toGMTString() + "\n"
+              + "   gasPrice        : " + e.gasPrice + "\n"
+              + "   gas             : " + e.gas + "\n"
+              + "   input           : " + e.input);
+          }
+        })
+      }
+    }
+  }
+  
